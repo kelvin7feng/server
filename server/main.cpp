25be6dcc9d91368d201e65f5dc059a94b6b919d0
kelvin <kelvin7.feng@gmail.com>
2016-10-10 21:35:28 +0800

    Initial Commit

---
+++ server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  server
+//
+//  Created by 冯文斌 on 16/10/10.
+//  Copyright © 2016年 kelvin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
